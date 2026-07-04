ae4a888573e08e324207c79ce17552c6eef5d357
Michael Kevin <141012017+TheRealLeszkon@users.noreply.github.com>
2026-07-04 12:04:10 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static void reverseSort(int[] arr) {
+        int temp;
+        for(int i =0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] =arr[j];
+                    arr[j]=temp;
+                }
+                
+            }
+        }
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int index =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[index++]=nums[i];
+            }
+        }
+        return index;
+
+    }
+}
